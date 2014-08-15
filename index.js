7af163db46432f2a491c8a127b260312ab816c7e
Rui Teixeira <rui.teixeira@webappuk.com>
2014-08-15 14:47:58 +0100

    Case 34002: Players can now invite each and play a game. End of game not checked !

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ console.log('Listening on Port ' + port);
 // Game variables
 global.onlinePlayers = new Array(); // Keeps sockets in a dictionary with key username
 global.playerNames = new Array(); // Keeps the usernames in a simple array
+global.games = new Array(); // An array of the games currently being played
 
 io.sockets.on('connection', function(socket) {
 	socket.emit('message', { message: 'welcome!'});
@@ -39,27 +40,129 @@ io.sockets.on('connection', function(socket) {
 		console.log('Registering player: ' + data.playerName);
 
 		// Check if the player name is valis and doesn't exist
-		if(data === '') {
+		if(data === '' || global.playerNames.indexOf(data.playerName) != -1) {
 			console.log('Registration Unsuccessful');
+			socket.emit('serverError', {message: 'Player already exists!'});
 			return;
 		}
 
+		// Store the player name
 		global.playerNames.push(data.playerName);
+
+		// Setup socket variables and store it
 		socket.username = data.playerName;
+		socket.opponent = '';
 		global.onlinePlayers[data.playerName] = socket;
-		socket.emit('successfulRegistration', {onlinePlayers: global.playerNames});
+
+		// Inform client it is registers
+		socket.emit('successfulRegistration');
+		// Send new list of players to all players
+		io.sockets.emit('onlinePlayers', {onlinePlayers: global.playerNames});
 	});
 
 	socket.on('invite', function(data) {
 		console.log(socket.username + ' is inviting ' + data.playerName + ' for a game!');
 
-		global.onlinePlayers[data.playerName].emit('message', {message: 'Invite!'});
+		if(data.playerName == '') {
+			return;
+		}
+
+		// Check if the player is not in game already
+		if(global.onlinePlayers[data.playerName].opponent !== '') {
+			socket.emit('serverError', {message: 'Players ' + data.playerName + ' is in game!'});
+			return;
+		}
+
+		global.onlinePlayers[data.playerName].emit('invite', {source: socket.username});
+	});
+
+	socket.on('acceptInvite', function(data) {
+		// CHECK IF USER IS STILL CONNECTED AND AVAILABLE
+		console.log(socket.username + ' accepted invite from ' + data.source);
+		global.onlinePlayers[data.source].emit('inviteAccepted', {'target': socket.username}); 
+		// VALIDATE IF BOTH PLAYERS ARE READY
+		var newGame  = new Game(socket.username, data.source);
+		var newLength = global.games.push(newGame);
+		newGame.startGame(newLength-1);
+		global.onlinePlayers[newGame.players[newGame.currentPlayerIndex]].emit('turn', {});
+	});
+
+	// Client sends request to make a move
+	// It sends the game id it belongs to and the move
+	socket.on('move', function(data) {
+		// CHECK IF THIS SOCKET CAN PLAY THIS GAME
+		console.log('MAKING A MOVE:')
+		console.log('Game ID: ' + data.gameID); // INITIALIZE THESE
+		console.log('Move: ' + '(' + data.move.row + ', ' + data.move.col + ')');
+		console.log('Current State: ' + global.games[data.gameID].table[data.move.row][data.move.col]);
+
+		var game = global.games[data.gameID];
+		if(game.move(socket.username, data.move)) {
+			for(var player = 0; player < global.games[data.gameID].players.length; player++) {
+				global.onlinePlayers[game.players[player]].emit(
+															'move', 
+														{	move: data.move, 
+															symbol: game.playerSymbols[game.currentPlayerIndex]
+														});
+			}
+			global.onlinePlayers[game.players[game.currentPlayerIndex]].emit('turn', {});
+		}
+		else {
+			console.log('Not valid!');
+		}
+		
 	});
 
 	socket.on('disconnect', function() {
+		// Check if the socket is registered
+		if(socket.username == null) {
+			console.log('An identified user disconnected! Socket info: ' + socket);
+			return;
+		}
 		var playerIndex = global.onlinePlayers.indexOf(socket.username);
 		global.onlinePlayers.splice(global.playerNames[playerIndex]);
 		global.playerNames.splice(playerIndex, 1);
 		console.log(socket.username + ' disconnected!');
 	});
 });
+
+// GAME CLASSS
+function Game(player1, player2) {
+	this.players = [player1, player2];
+	this.playerSymbols = ['X', 'O']
+	this.currentPlayerIndex = 0; // Initialize to player 1 to avoid possiblle initilization problems
+	this.table = new Array();
+
+	this.startGame = function(gameID)  {
+		this.table = [
+						[0 , 0, 0],
+						[0 , 0, 0],
+						[0 , 0, 0]];
+		for(var player = 0; player < this.players.length; player++) {
+			global.onlinePlayers[this.players[player]].emit('startGame', {gameID: gameID, opponent: this.players[player]});
+		}
+	};
+
+	this.setInitialPlayer = function(player) {
+		this.currentPlayer = player;
+	}
+
+	this.getNextPlayer = function() {
+		this.currentPlayerIndex++;
+		if(this.currentPlayerIndex >= this.players.length) {
+			this.currentPlayerIndex = 0;
+		}
+	}
+
+	// TODO: Check if player can play in this game
+	this.move = function(player, move) {
+		if(this.table[move.row][move.col] === 0 && player === this.players[this.currentPlayerIndex]) {
+			this.table[move.row][move.col] = player;
+			this.getNextPlayer();
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -25,11 +25,74 @@ socket.on('message', function(data) {
 // Data contains an array of online users
 socket.on('successfulRegistration', function(data) {
 	$('#startForm').hide();
-	loadOnlinePlayers(data.onlinePlayers);
-	console.log(data);
+	$('#onlinePlayers').show();
+});
+
+// Server warns something went wrong and the action was not carried
+// Data contains error message {message}
+socket.on('serverError', function(data) {
+	window.alert(data.message);
+});
+
+// Server sends an updated list of players
+// Data contains an array with all the online players {onlinePlayers}
+socket.on('onlinePlayers', function(data) {
+	$('#opponentSelect').empty();
+	for(player in data.onlinePlayers) {
+		if(data.onlinePlayers[player] != socket.username) {
+			$('#opponentSelect').append('<option>' + data.onlinePlayers[player] + '</option>');
+		}
+	}
+});
+
+// Server warns the client someeone invited it to a game
+// Data contains the plaer username {source}
+socket.on('invite', function(data) {
+	var accept = window.confirm('Player ' + data.source + ' Just invited you to a game!'
+				+ ' \nDo you wish to accept?');
+
+	if(accept) {
+		socket.emit('acceptInvite', {source: data.source});
+	}
+	else {
+		console.log('decline');
+	}
+});
+
+// Server requests the server to start the game
+// Returns the game id {gameID} and the name of the opponent {opponent}
+socket.on('startGame', function(data) {
+	$('#mainInfo').show();
+	$('#playerName').text(socket.username);
+	$('#opponentName').text(data.opponent);
+
+	$('#onlinePlayers').hide();
+	$('#gameTable').show();
+
+	socket.gameID = data.gameID;
+	socket.opponent = data.opponent;
+});
+
+// Server warns the client that its inviation was successful
+// Data contains the name of the player {target} for the client to confirm
+socket.on('inviteAccepted', function(data) {
+	console.log(data.target + 'Accepted');
+});
+
+// Server authorizes the client to make a move
+// Data containers the move {move: {col, row}}
+socket.on('move', function(data) {
+	$('#status').text('Waiting for opponent\'s move');
+	$('#cell-' + data.move.row + '-' + data.move.col + ' a').text(data.symbol);
 });
 
 
+// Server warns the client that it is its turn
+socket.on('turn', function(data) {
+	$('#status').text('Your turn!');
+});
+
+// Store this a in a player management file
 function send() {
 	socket.emit('send', {message: 'This was sent by the server'});
 }
@@ -37,16 +100,11 @@ function send() {
 function register()  {
 	if (startForm.nameInput.value === '') {
 		window.alert('Please input a user name!');
+		// Stop execution -- NOT DONE FOR TESTING PURPOSES
 	}
-	socket.emit('register', {playerName: startForm.nameInput.value})
+	socket.username = startForm.nameInput.value;
+	socket.emit('register', {playerName: socket.username})
 	return false;
-}
-
-function loadOnlinePlayers(playersList) {
-	for(player in playersList) {
-		$('#opponentSelect').append('<option>' + playersList[player] + '</option>');
-	}
-	$('#onlinePlayers').show();
 }
 
 function refreshOnlinePlayers() {
@@ -55,6 +113,15 @@ function refreshOnlinePlayers() {
 
 function invitePlayer() {
 	var opponent = $('#opponentSelect').find(':selected').text();
-	
 	socket.emit('invite', {playerName: opponent});
+}
+
+function play(link) {
+	var idSplit; // Array
+	idSplit = link.parentNode.id.split('-');
+	socket.emit('move', {	gameID: socket.gameID,
+							move: {	row: idSplit[1], 
+									col: idSplit[2]
+								}
+							});
 }
